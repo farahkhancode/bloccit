550041078f95aaf1d40f32e33e93d283cf9534ba
farahmed@gmail.com <farahmed@gmail.com>
2018-07-09 21:01:44 -0500

    WIP deploy to production

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,6 +2,5 @@ require("dotenv").config();
 
 module.exports = {
   init(){
-
   }
 };
--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ function normalizePort(val) {
   return false;
 }
 
+
 server.on("listening", () => {
   console.log(`server is listening for requests on port ${server.address().port}`);
 });
