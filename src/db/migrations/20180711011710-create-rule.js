d1adc29512631c45c0fdb253119aee06d91202eb
farahmed@gmail.com <farahmed@gmail.com>
2018-07-10 20:27:13 -0500

    assignment-5 complete

---
+++ src/db/migrations/20180711011710-create-rule.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Rule', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+
+      topicId: {
+      type: Sequelize.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+      model: "Topic",
+      key: "id",
+      as: "topicId",
+      },
+     }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Rule');
+  }
+};
