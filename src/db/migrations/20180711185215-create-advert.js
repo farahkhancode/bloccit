00ba397beb9aaa4779e18dd0ce68111bea4f9eff
farahmed@gmail.com <farahmed@gmail.com>
2018-07-13 19:08:27 -0500

    topics and adverts loading-seeding working

--- src/db/migrations/20180711185215-create-advert.js
+++ src/db/migrations/20180711185215-create-advert.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Advert', {
+    return queryInterface.createTable('Adverts', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Advert');
+    return queryInterface.dropTable('Adverts');
   }
 };
