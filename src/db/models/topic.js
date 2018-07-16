41e2882c1585e983809de9595877ba363565eb45
farahmed@gmail.com <farahmed@gmail.com>
2018-07-16 15:16:38 -0500

    jasmine tests passing

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -2,15 +2,25 @@
 
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+	type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+       type: DataTypes.STRING,
+       allowNull: false
+    }
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicId",
+      as: "posts"
+    });
     Topic.hasMany(models.Banner, {
     foreignKey: "topicId",
     as: "banners",
-  }),
+  });
   Topic.hasMany(models.Rule, {
     foreignKey: "topicId",
     as: "rules",
