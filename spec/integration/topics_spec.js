df00bf4dcb0965c46919b91250e7b93b00effb21
farahmed@gmail.com <farahmed@gmail.com>
2018-07-18 00:29:51 -0500

    topic validation

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -1,19 +1,18 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/topics/";
-
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 
 describe("routes : topics", () => {
 
-  beforeEach((done) => {
+   beforeEach((done) => {
      this.topic;
      sequelize.sync({force: true}).then((res) => {
 
       Topic.create({
         title: "JavaScript Frameworks",
-        description: "There is a lot of them"
+        description: "There are a lot of them"
       })
        .then((topic) => {
          this.topic = topic;
@@ -28,19 +27,17 @@ describe("routes : topics", () => {
 
    });
 
-
   describe("GET /topics", () => {
-    it("should return a status code 200 and all topics", (done) => {
 
-   //#3
-          request.get(base, (err, res, body) => {
-            expect(res.statusCode).toBe(200);
-            expect(err).toBeNull();
-            expect(body).toContain("Topics");
-            expect(body).toContain("JavaScript Frameworks");
-            done();
-          });
-        });
+    it("should return a status code 200 and all topics", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(err).toBeNull();
+        expect(body).toContain("Topics");
+        expect(body).toContain("JavaScript Frameworks");
+        done();
+      });
+    });
   });
 
   describe("GET /topics/new", () => {
@@ -55,69 +52,84 @@ describe("routes : topics", () => {
 
   });
 
+  describe("POST /topics/create", () => {
+    const options = {
+      url: `${base}create`,
+      form: {
+        title: "blink-182 songs",
+        description: "What's your favorite blink-182 song?"
+      }
+    };
+
+    it("should create a new topic and redirect", (done) => {
+
+      request.post(options,
+
+        (err, res, body) => {
+          Topic.findOne({where: {title: "blink-182 songs"}})
+          .then((topic) => {
+
+            expect(res.statusCode).toBe(303);
+            expect(topic.title).toBe("blink-182 songs");
+            expect(topic.description).toBe("What's your favorite blink-182 song?");
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+
+    it("should not create a new topic that fails validations", (done) => {
+      const options = {
+        url: `${base}create`,
+        form: {
+          title: "a",
+          description: "b"
+        }
+      };
+
+      request.post(options,
+        (err, res, body) => {
+          Topic.findOne({where: {title: "a"}})
+          .then((topic) => {
+              expect(topic).toBeNull();
+              done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    });
+  });
+
   describe("GET /topics/:id", () => {
 
-     it("should render a view with the selected topic", (done) => {
-       request.get(`${base}${this.topic.id}`, (err, res, body) => {
-         expect(err).toBeNull();
-         expect(body).toContain("JavaScript Frameworks");
-         done();
-       });
-     });
-
-   });
-   describe("GET /topics/:id/edit", () => {
-    it("should render a view with an edit topic form", (done) => {
-      request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
+    it("should render a view with the selected topic", (done) => {
+      request.get(`${base}${this.topic.id}`, (err, res, body) => {
         expect(err).toBeNull();
-        expect(body).toContain("Edit Topic");
         expect(body).toContain("JavaScript Frameworks");
         done();
       });
     });
 
   });
-  describe("POST /topics/:id/update", () => {
-     it("should update the topic with the given values", (done) => {
-        const options = {
-           url: `${base}${this.topic.id}/update`,
-           form: {
-             title: "JavaScript Frameworks",
-             description: "There are a lot of them"
-           }
-         };
-//#1
-         request.post(options,
-           (err, res, body) => {
 
-           expect(err).toBeNull();
-//#2
-           Topic.findOne({
-             where: { id: this.topic.id }
-           })
-           .then((topic) => {
-             expect(topic.title).toBe("JavaScript Frameworks");
-             done();
-           });
-         });
-     });
-
-   });
-
-   describe("POST /topics/:id/destroy", () => {
+  describe("POST /topics/:id/destroy", () => {
 
     it("should delete the topic with the associated ID", (done) => {
 
-//#1
       Topic.all()
       .then((topics) => {
 
-//#2
         const topicCountBeforeDelete = topics.length;
 
         expect(topicCountBeforeDelete).toBe(1);
 
-//#3
         request.post(`${base}${this.topic.id}/destroy`, (err, res, body) => {
           Topic.all()
           .then((topics) => {
@@ -133,36 +145,45 @@ describe("routes : topics", () => {
 
   });
 
-  describe("POST /topics/create", () => {
-     const options = {
-       url: `${base}create`,
-       form: {
-         title: "blink-182 songs",
-         description: "What's your favorite blink-182 song?"
-       }
-     };
+  describe("GET /topics/:id/edit", () => {
 
-     it("should create a new topic and redirect", (done) => {
+    it("should render a view with an edit topic form", (done) => {
+      request.get(`${base}${this.topic.id}/edit`, (err, res, body) => {
+        expect(err).toBeNull();
+        expect(body).toContain("Edit Topic");
+        expect(body).toContain("JavaScript Frameworks");
+        done();
+      });
+    });
 
-//#1
-       request.post(options,
+  });
 
-//#2
-         (err, res, body) => {
-           Topic.findOne({where: {title: "blink-182 songs"}})
-           .then((topic) => {
-             expect(res.statusCode).toBe(303);
-             expect(topic.title).toBe("blink-182 songs");
-             expect(topic.description).toBe("What's your favorite blink-182 song?");
-             done();
-           })
-           .catch((err) => {
-             console.log(err);
-             done();
-           });
-         }
-       );
-     });
-   });
+  describe("POST /topics/:id/update", () => {
+
+    it("should update the topic with the given values", (done) => {
+       const options = {
+          url: `${base}${this.topic.id}/update`,
+          form: {
+            title: "JavaScript Frameworks",
+            description: "There are a lot of them"
+          }
+        };
+
+        request.post(options,
+          (err, res, body) => {
+
+          expect(err).toBeNull();
+
+          Topic.findOne({
+            where: { id: this.topic.id }
+          })
+          .then((topic) => {
+            expect(topic.title).toBe("JavaScript Frameworks");
+            done();
+          });
+        });
+    });
+
+  });
 
 });
